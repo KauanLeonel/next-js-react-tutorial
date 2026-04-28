208f799afb467da059acdcd4bedbab13a3e2d2d4
Kauan Leonel <kauan.br.char@gmail.com>
2026-04-27 22:13:04 -0300

    Class 5 Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,39 @@
+'use client'
+
+
+import Link from "next/link";
 import Logo from "./Logo";
 import { GiFrogFoot } from "react-icons/gi";
+import { useRouter } from 'next/navigation'
+ 
 
 export default function Header() {
+
+  const router = useRouter();
+
   return (
-    <header  style={styles.container}>
+    <header style={styles.container}>
       <Logo></Logo>
       <nav>
         <ul style={styles.nav}>
-          <li style={styles.logo}>
-            <GiFrogFoot />
+          <li style={styles.logo} onClick={() => router.push('/')} >
+            <GiFrogFoot  />
             Home
-          </li>
-          <li style={styles.logo}>
+            </li>
+          <li style={styles.logo} onClick={() => router.push('/dinossaurs')}>
             <GiFrogFoot />
-            Products
+            Dinossaurs
           </li>
-          <li style={styles.logo}>
+          <li style={styles.logo} onClick={() => router.push('/signup')}>
             <GiFrogFoot />
-            About
-          </li>
+              Signup
+            </li>
           <li style={styles.logo}>
-            <GiFrogFoot />
-            Contacts
-          </li>
+                
+            <GiFrogFoot /><Link href="/profiles">
+              Profiles</Link>
+           
+            </li>
         </ul>
       </nav>
     </header>
@@ -42,11 +53,12 @@ const styles = {
     display: "flex",
     listStyleType: "none",
     gap: 20,
+      cursor: "pointer"
   },
   logo: {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    gap: 10
+    gap: 10,
   },
 };
